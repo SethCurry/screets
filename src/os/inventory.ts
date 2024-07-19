a0258f19a068ada09ebea8a2a97ad15e1177c429
Seth Curry <seth@scurry.io>
2024-07-19 01:37:15 -0500

    Adding some comments explaining what code does

--- src/os/inventory.ts
+++ src/os/inventory.ts
@@ -1,5 +1,12 @@
+// inventory.ts contains functions for working with the in-game "inventory" such
+// as creeps, objects in rooms, etc.
+
+// CreepFilter defines a type that can be used to filter creeps.
 export type CreepFilter = (creeps: Creep) => boolean;
 
+// creepsArray returns an array of all creeps in the game.
+// This is a convenience function to allow map and forEach
+// to be used.
 export function creepsArray(): Creep[] {
   const creeps: Creep[] = [];
 
@@ -10,10 +17,14 @@ export function creepsArray(): Creep[] {
   return creeps;
 }
 
+// hasNoIntent is a CreepFilter that returns true for creeps that
+// have no intent set.
 export const hasNoIntent: CreepFilter = (creep: Creep) => {
   return creep.memory.intent === undefined
 }
 
+// hasAtLeastBodyParts is a CreepFilter that returns true for creeps with at least the given number
+// of the specified body part.
 export function hasAtLeastBodyParts(part: BodyPartConstant, partCount: number): CreepFilter {
   const impl = (creep: Creep) => {
     return creep.getActiveBodyparts(part) >= partCount
@@ -22,12 +33,16 @@ export function hasAtLeastBodyParts(part: BodyPartConstant, partCount: number): 
   return impl
 }
 
+// filterCreeps filters an array of creeps using the provided filters.  The filters will be and-ed
+// together.
 export function filterCreeps(creeps: Creep[], ...filters: CreepFilter[]): Creep[] {
   return creeps.filter((c) => {
     filters.map(f => f(c)).filter(v => v === false).length === 0;
   })
 }
 
+// filterAllCreeps is a convenience function that runs filterCreeps on all of the creeps
+// in the game.
 export function filterAllCreeps(...filters: CreepFilter[]): Creep[] {
   return filterCreeps(creepsArray(), ...filters)
 }
